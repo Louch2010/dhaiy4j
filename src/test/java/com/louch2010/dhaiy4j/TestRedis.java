f671cadd20e3ab0be361a056d34efe575d7da815
Louch2010 <luocihang@126.com>
2016-09-14 16:54:04 +0800

    优化

--- src/test/java/com/louch2010/dhaiy4j/TestRedis.java
+++ src/test/java/com/louch2010/dhaiy4j/TestRedis.java
@@ -1,15 +1,13 @@
 package com.louch2010.dhaiy4j;
 
-import redis.clients.jedis.Jedis;
-
 public class TestRedis {
 	public static void main(String[] args) {
-		Jedis jedis = new Jedis("127.0.0.1", 6379);
+		/*Jedis jedis = new Jedis("127.0.0.1", 6379);
 		long start = System.currentTimeMillis();
 		for (int i = 0; i < 10000; i++) {
 			jedis.set("key_1_" + i, "value_" + i);
 		}
 		long end = System.currentTimeMillis();
-		System.out.println("耗时："+ (end - start));
+		System.out.println("耗时："+ (end - start));*/
 	}
 }
