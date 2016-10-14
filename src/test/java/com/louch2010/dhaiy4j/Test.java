d9976b4885c67eb57280f5685d018a98d75833b3
Louch2010 <1175889450@qq.com>
2016-10-14 19:21:43 +0800

    集成spring测试

--- src/test/java/com/louch2010/dhaiy4j/Test.java
+++ src/test/java/com/louch2010/dhaiy4j/Test.java
@@ -3,7 +3,7 @@ package com.louch2010.dhaiy4j;
 public class Test {
 	public static void main(String[] args) throws Exception {
 		Dhaiy client = new Dhaiy("127.0.0.1", 1334);
-		System.out.println(client.exist("bbb"));
+		/*System.out.println(client.exist("bbb"));
 		System.out.println(client.set("aaa", "罗词航中华人民共和国"));
 		//
 		System.out.println(client.exist("aaa"));
@@ -13,12 +13,12 @@ public class Test {
 		System.out.println(client.get("aaa"));
 		System.out.println(client.delete("aaa"));
 		
-		System.out.println(client.get("name"));
+		System.out.println(client.get("name"));*/
 		
 		
 		
 		
-		/*long start = System.currentTimeMillis();
+		long start = System.currentTimeMillis();
 		for (int i = 0; i < 10000; i++) {
 			int time = (int) (10 * Math.random());
 			if (time ==0){
@@ -27,7 +27,7 @@ public class Test {
 			client.set("key_1_" + i, "value_" + i);
 		}
 		long end = System.currentTimeMillis();
-		System.out.println("耗时："+ (end - start));*/
+		System.out.println("耗时："+ (end - start));
 		
 		client.close();
 	}
